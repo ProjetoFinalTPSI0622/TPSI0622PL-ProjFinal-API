ec0b92b3c96c240ee54be7878eb7206ec09f10b1
Fábio <fabiomiguel3.10@gmail.com>
2023-12-29 22:41:54 +0000

    Added roles and user seeder

--- backendAPI/database/seeds/RolesSeeder.php
+++ backendAPI/database/seeds/RolesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolesSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class RolesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('roles')->insert([
+            'name' => 'admin',
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'user',
+        ]);
     }
 }
