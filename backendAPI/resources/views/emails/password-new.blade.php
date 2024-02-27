66196480f39aad6e15295513818fae23658df752
PedroNito <Pedro.nito.23@gmail.com>
2024-02-27 21:04:52 +0000

    solving reset email

--- backendAPI/resources/views/emails/password-new.blade.php
+++ backendAPI/resources/views/emails/password-new.blade.php
@@ -476,9 +476,7 @@
                                                                                                                             nova
                                                                                                                             password
                                                                                                                             é:
-                                                                                                                            "
-                                                                                                                            monengue
-                                                                                                                            "
+                                                                                                                            <span class="font-weight-bold text-center">O seu NIF</span>
                                                                                                                         </p>
                                                                                                         </td>
                                                                                                     </tr>
@@ -534,7 +532,7 @@
                                                                                                         <td class="t103"
                                                                                                             style="background-color:#312052;overflow:hidden;width:326px;text-align:center;line-height:50px;mso-line-height-rule:exactly;mso-text-raise:10px;border-radius:14px 14px 14px 14px;">
                                                                                                             <a class="t109"
-                                                                                                                href="https://tabular.email"
+                                                                                                                href="https://cesaedesk.atec-porto.eu/login"
                                                                                                                 style="display:block;margin:0;Margin:0;font-family:BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif,'Inter';line-height:50px;font-weight:600;font-style:normal;font-size:18px;text-decoration:none;direction:ltr;color:#FFFFFF;text-align:center;mso-line-height-rule:exactly;mso-text-raise:10px;"
                                                                                                                 target="_blank">Iniciar
                                                                                                                 sessão</a>
