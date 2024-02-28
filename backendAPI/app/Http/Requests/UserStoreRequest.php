738673d285d38575a2c4c51058f509af5e8cdc03
daniel pereira <danielpereira22costa@gmail.com>
2024-02-28 03:44:43 +0000

    Update UserStoreRequest.php

--- backendAPI/app/Http/Requests/UserStoreRequest.php
+++ backendAPI/app/Http/Requests/UserStoreRequest.php
@@ -25,7 +25,7 @@ class UserStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|max:255',
+            'name' => 'required|max:60|regex:/^[a-zA-Z\s]*$/',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|max:255',
             'internalcode' => 'required|max:255',
@@ -38,19 +38,20 @@ class UserStoreRequest extends FormRequest
      *
      * @return array
      */
-     public function messages()
-     {
-         return [
-             'name.required' => 'O nome é obrigatório',
-             'name.max'  => 'O nome deve ter menos de 255 caracteres',
-             'email.required' => 'O email é obrigatório',
-             'email.email' => 'O email deve ser um email válido',
-             'email.unique' => 'O email já existe',
-             'email.max' => 'O email deve ter menos de 255 caracteres',
-             'password.required' => 'A password é obrigatória',
-             'password.max' => 'A password deve ter menos de 255 caracteres',
-             'internalcode.required' => 'O código interno é obrigatório',
-             'internalcode.max' => 'O código interno deve ter menos de 255 caracteres',
-         ];
-     }
+    public function messages()
+    {
+        return [
+            'name.required' => 'O nome é obrigatório',
+            'name.max' => 'O nome deve ter menos de 60 caracteres',
+            'name.regex' => 'O nome deve conter apenas letras e espaços',
+            'email.required' => 'O email é obrigatório',
+            'email.email' => 'O email deve ser um email válido',
+            'email.unique' => 'O email já existe',
+            'email.max' => 'O email deve ter menos de 255 caracteres',
+            'password.required' => 'A password é obrigatória',
+            'password.max' => 'A password deve ter menos de 255 caracteres',
+            'internalcode.required' => 'O código interno é obrigatório',
+            'internalcode.max' => 'O código interno deve ter menos de 255 caracteres',
+        ];
+    }
 }
